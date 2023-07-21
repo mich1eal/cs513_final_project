720086789a7dcc2ac72fa1ed77304b91933a0a9a
mich1eal <msmille3@gmail.com>
2023-07-21 14:04:59 -0600

    Unzip and read w pandas

--- src/clean_dataset.py
+++ src/clean_dataset.py
@@ -5,5 +5,18 @@ Michael Miller and Shane Sepac for University of Illinois CS 513
 7/18/2023
 """
 
+from os import path
+import zipfile
+import pandas as pd
 
-#@todo write this
+PATH_RAW = path.join('..', 'data,', 'raw')
+RAW_ZIP = 'Food_Inspections.zip'
+RAW_CSV = 'Food_Inspections.csv'
+
+# load raw data into dataframe
+
+zipped = zipfile.ZipFile(path.join(PATH_RAW, RAW_ZIP))
+
+raw_df = pd.read_csv(zipped.open(RAW_CSV))
+
+

575fddff26eed3fd1e5c2f2d0ff783de91975ea6
mich1eal <msmille3@gmail.com>
2023-07-23 13:34:32 -0600

    Validation framework works

--- src/assertion_chain.py
+++ src/assertion_chain.py
@@ -2,8 +2,8 @@
 Michael Miller and Shane Sepac for University of Illinois CS 513
 7/18/2023
 """
-import Pandas as pd 
-
+import pandas as pd
+from os import path, listdir, remove, makedirs
 
 class AssertionChain:
     """
@@ -17,68 +17,127 @@ class AssertionChain:
         validate - determines whether a validate meets all asserts 
     """
     
-    def __init__(self):
+    def __init__(self, out_path: str, explore_path: str):
         self.assertion_chain = []
-    
-    
-    def add_assertion(self, name: str, clean_assert: callable, resolve: callable):
-        """
-        Assert will be applied as part of this AssertionChain. Note that order matters!
         
-
-        Parameters
-        ----------
-        clean_assert : callable
-            
-            
-        resolve : callable
-            called 
-
-        Returns
-        -------
-        None.
-
-        """
-        self.assertion_chain.append((name, clean_assert, resolve))
+        # set up directories. Clear them, create if not exists         
+        self.out_path = out_path
+        self.explore_path = explore_path
         
-    
-    def explore(self, df: pd.Dataframe):
+        for directory in (out_path, explore_path):    
+            if path.exists(directory):
+            # Clear the directory
+                for filename in listdir(directory):
+                    file_path = path.join(directory, filename)
+                    remove(file_path)
+                print(f"Directory '{directory}' cleared.")
+            else:
+                # Create the directory
+                makedirs(directory)
+                print(f"Directory '{directory}' created.")
+     
+    def _get_failures(self, df: pd.DataFrame, assertion: dict) -> pd.DataFrame:
+        """
+        Apply an assertion, return rows that fail the assertion
+        """
+        valid_idx = assertion['clean_assert'](df)
+        fail_rows = df[~valid_idx]
+        return fail_rows
+         
+    def _resolve_failures(self, df: pd.DataFrame, assertion: dict) -> pd.DataFrame:
+        """
+        Applies this assertions resolution function
+        """
+        valid_idx = assertion['clean_assert'](df)
+        
+        # for drop we simply select valid rows
+        if assertion['operation'] == 'drop':
+            df = df[valid_idx]
+        
+        # for apply, we apply resolve function to invalid rows 
+        elif assertion['operation'] == 'apply':
+            df[~valid_idx] = assertion['resolve'](df[~valid_idx])
+            
+        return df
+     
+    def _validate_assertion(self, df: pd.DataFrame, assertion: dict):
+         """
+         Throws assertion error if assertion is not met 
+         """
+         fail_count = len(self._get_failures(df, assertion).index)
+         assert fail_count == 0, f'Assertion {assertion["name"]} failed {fail_count} times'
+       
+    def add(self, name: str, clean_assert: callable, operation:str='drop', resolve:callable=None):
+        """
+        The new assertion will be applied as part of this AssertionChain. Note that order matters!
+        """
+        assert operation in ('drop', 'apply'), "'Operation must be either 'drop' or 'apply'"
+
+        self.assertion_chain.append({'name': name,
+                                     'operation': operation,
+                                     'clean_assert': clean_assert,
+                                     'resolve': resolve})
+   
+    def explore_chain(self, df: pd.DataFrame) -> pd.DataFrame():
+        """
+        Applies each assertion in the chain, outputting failed rows for each assertion
+        to explore_path. 
+        Saves final claned data in explore_path and returns it
+        """
+        print('EXPORING DATASET')
         
         for i, assertion in enumerate(self.assertion_chain):
-            name, clean_assert, resolve = assertion 
-            print(f'Assertion {i}: {name}:\n')
+            print(f'Assertion {i}: {assertion["name"]}:')
             
             # apply function to dataframe
-            valid_idx = clean_assert(df)
-            fail_rows = df[~valid_idx]
+            fail_rows = self._get_failures(df, assertion)
             
             # calculate metrics about this assertion
             fail_count = len(fail_rows.index)
             fail_percent = 100 * fail_count / len(df.index)
             fail_file = f'assertion{i}_failed_rows.csv'
             
-            print(f'/t{fail_count} rows fail ({fail_percent}%). Failed rows saved as {fail_file}')
-            fail_rows.to_csv(fail_file)
-           
-        
-        
+            # write output
+            print(f'\t{fail_count} rows fail ({fail_percent:.2f}%)')
+            print(f'\tFailed rows saved as {fail_file}')
+            fail_rows.to_csv(path.join(self.explore_path, fail_file))
+            
+            # resolve failures so we can proceed to next assertion
+            print('\tApplying resolution function')
+            df = self._resolve_failures(df, assertion)
+            print(f'\tRemaining rows: {len(df.index)}')
             
         
+        # output assertion log for reference 
+        assertion_log = pd.DataFrame(self.assertion_chain)
+        assertion_log = assertion_log['name']
+        assertion_log.rename_axis('Assertion', inplace=True)
+        assertion_log.to_csv(path.join(self.explore_path, 'assertions.csv'))
         
-        
-        
-        
-        
-        
-        
+        # output the cleaned data
+        df.to_csv(path.join(self.out_path, 'exploration_results.csv'))
+        return df
     
+    def apply_chain(self, df: pd.DataFrame) -> pd.DataFrame():
+        """
+        Applies assertion chain, outputs cleaned data and also returns it 
+        """
+        print('APPLYING ASSERTION CHAIN')
         
-    
-        
-        
-        
-        
-        
-        
-        
-        
+        assertion_count = len(self.assertion_chain)
+        for i, assertion in enumerate(self.assertion_chain):
+            print(f'\tApplying assertion {i} of {assertion_count}')
+            df = self._resolve_failures(df, assertion)
+            
+        # output the cleaned data
+        df.to_csv(path.join(self.out_path, 'cleaned_data.csv'))
+        return df
+
+    def validate_chain(self, df: pd.DataFrame) -> pd.DataFrame():
+        """
+        Applies assertion chain, outputs cleaned data and also returns it 
+        """
+        print('VALIDATING DATASET')
+        for i, assertion in enumerate(self.assertion_chain):
+            print(f'\tAsserting: {assertion["name"]}')
+            self._validate_assertion(df, assertion)
--- src/clean_dataset.py
+++ src/clean_dataset.py
@@ -8,15 +8,36 @@ Michael Miller and Shane Sepac for University of Illinois CS 513
 from os import path
 import zipfile
 import pandas as pd
+from assertion_chain import AssertionChain
 
-PATH_RAW = path.join('..', 'data,', 'raw')
+
+# constant paths and files 
+PATH_RAW = path.join('..', 'data', 'raw')
 RAW_ZIP = 'Food_Inspections.zip'
 RAW_CSV = 'Food_Inspections.csv'
 
+PATH_OUT = path.join('..', 'data', 'processed')
+PATH_LOG = path.join('..', 'data', 'log')
+
 # load raw data into dataframe
-
 zipped = zipfile.ZipFile(path.join(PATH_RAW, RAW_ZIP))
-
 raw_df = pd.read_csv(zipped.open(RAW_CSV))
 
+# set up AssertionCHain for cleaning 
+chain = AssertionChain(out_path=PATH_OUT, explore_path=PATH_LOG)
+
+# add assertions that we want to check, and functions to resolve (drop by default)
+license_not_null = lambda df: ~df['License #'].isnull() & ~df['License #'].isna()
+chain.add(name='License not null', clean_assert=license_not_null)
+
+chain.add(name='License > 0', clean_assert=lambda df: df['License #'] > 0)
+
+# explore assertion chain to understand impacts on dataset
+chain.explore_chain(raw_df)
+
+# run assertion chain
+cleaned_df = chain.apply_chain(raw_df)
+
+# validate assertion chain
+chain.validate_chain(cleaned_df)
 
